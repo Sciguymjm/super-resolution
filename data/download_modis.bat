891ee6443d37f78b8c03aa7bb42ea10e29e859d8
M <sciguymjm@gmail.com>
2017-07-11 12:58:19 -0700

    it runs
    Former-commit-id: 715434e3bfd142ca723f2e9cd950687d05e84dae [formerly 54194d4a7a50ac4b2636302c8338890ff5f5fa2d]
    Former-commit-id: 2c814656cc8a69cd9def0428bfda1469126e6abc
    Former-commit-id: 73e12c05f9dcfd29a829ef8798dac994a101bf18

--- data/download_modis.bat
+++ data/download_modis.bat
@@ -1,3 +1,2 @@
-
-Daac2Disk_win.exe --shortname MCD43B3 --versionid 005 --begin 2017-03-06 --end 2017-03-06 --outputdir modis --tile 28 29 4 5 < input.txt
+Daac2Disk_win.exe --shortname MOD09A1 --versionid 005 --begin 2017-03-06 --end 2017-03-06 --outputdir modis --tile 28 29 4 5
 PAUSE
