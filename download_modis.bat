359b4d9610c7d3de39556811d87fa11b0b7d265a
M <sciguymjm@gmail.com>
2017-07-11 10:12:09 -0700

    First commit
    Former-commit-id: 7aef209f2155169b5c67840e3666f9e254d3b9a4

---
+++ download_modis.bat
@@ -0,0 +1,3 @@
+
+Daac2Disk_win.exe --shortname MCD43B3 --versionid 005 --begin 2017-03-06 --end 2017-03-06 --outputdir modis --tile 28 29 4 5 < input.txt
+PAUSE
---
+++ download_modis.py
@@ -0,0 +1,14 @@
+from subprocess import Popen, PIPE, STDOUT
+
+year = "2017"
+month = "03"
+day = "06"
+
+username = "mmage"
+password = "ue3AaxCIb23I"
+
+p = Popen(["Daac2Disk_win.exe", "--shortname", "MCD43B3", "--versionid", "005",
+           "--begin", "{0}-{1}-{2}".format(year, month, day), "--end", "{0}-{1}-{2}".format(year, month, day),
+           "--outputdir", "modis", "--tile", "28", "29", "4", "5"], stdin=PIPE)
+stdout = p.communicate("y\n" + username+"\n" + password+"\n"])
+print stdout
