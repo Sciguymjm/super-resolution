0cf4a1d05598002f14c8d6ee57c90cce6bbc6116
M <sciguymjm@gmail.com>
2017-07-13 11:32:53 -0700

    Added initial modis processing + some himwari data
    Former-commit-id: d9417f1b6c6235d980a061683a813d9228f1030f [formerly af31dd25c421dd750788348808ee360959a0f394] [formerly 467a46fb5f1bbada466adf97b06716cc5d01e2b0] [formerly abf8066d2c10203254a3d6c09d76b52e50e8fc19 [formerly f54b287e9d169d07544ff52a364c5651aa2aef76]]
    Former-commit-id: d9417f1b6c6235d980a061683a813d9228f1030f [formerly af31dd25c421dd750788348808ee360959a0f394] [formerly 467a46fb5f1bbada466adf97b06716cc5d01e2b0]
    Former-commit-id: 2ed028580fdefae08d4e9b73439cbb8ea94ffcab [formerly 8fc44fd2a38554ab1fff65744149336f873a3a14]
    Former-commit-id: e756d7bc678279eb552528a455fc3bec94875f78
    Former-commit-id: 29d6979084f0ccf824e40df301a2a9d1dafd2fec

--- data/download_modis.bat
+++ data/download_modis.bat
@@ -1,2 +1,2 @@
-Daac2Disk_win.exe --shortname MOD09A1 --versionid 005 --begin 2017-03-06 --end 2017-03-06 --outputdir modis --tile 28 29 4 5
+Daac2Disk_win.exe --shortname MOD13Q1 --versionid 005 --begin 2017-03-06 --end 2017-03-06 --outputdir modis --tile 28 29 4 5
 PAUSE
