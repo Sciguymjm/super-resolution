f581f1197af91888fb889bedae6e32ae5d461d0f
M <sciguymjm@gmail.com>
2017-07-14 14:11:01 -0700

    Fixed formatting
    Former-commit-id: 5f62a216389c18a14bda97c0d8b7a59e10e0ad75

--- data/download_modis.py
+++ data/download_modis.py
@@ -1,4 +1,4 @@
-from subprocess import Popen, PIPE, STDOUT
+from subprocess import Popen, PIPE
 
 year = "2017"
 month = "03"
@@ -10,5 +10,5 @@ password = "ue3AaxCIb23I"
 p = Popen(["Daac2Disk_win.exe", "--shortname", "MCD43B3", "--versionid", "005",
            "--begin", "{0}-{1}-{2}".format(year, month, day), "--end", "{0}-{1}-{2}".format(year, month, day),
            "--outputdir", "modis", "--tile", "28", "29", "4", "5"], stdin=PIPE)
-stdout = p.communicate("y\n" + username+"\n" + password+"\n")
+stdout = p.communicate("y\n" + username + "\n" + password + "\n")
 print stdout
