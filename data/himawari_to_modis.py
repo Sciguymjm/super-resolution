16d255f0e67969b152c986f2a6b936400b9b702b
M <sciguymjm@gmail.com>
2017-07-19 11:04:44 -0700

    Misc fixes
    Former-commit-id: 85e20ffd908b74328b780ea14dbaf665ea01d0cf [formerly 5c102eef648a5940a96f957f352e0f313cb31ee9] [formerly f8cd3cc0d43c716a9ce1975bf32e2bc05bddcc0e] [formerly 67ecb1e10421e8860305ae01c7cce0004aedc87b [formerly 7d79e97e4850bff4def96c0bb20a70cb7dd9a4cf]]
    Former-commit-id: 85e20ffd908b74328b780ea14dbaf665ea01d0cf [formerly 5c102eef648a5940a96f957f352e0f313cb31ee9] [formerly f8cd3cc0d43c716a9ce1975bf32e2bc05bddcc0e]
    Former-commit-id: fefcf274b3289ac9c8c409caa2d9346cc96aaea6 [formerly 42c6096b1196fb96054271b9a10152e56ebe3938]
    Former-commit-id: bf17bbe651dba5a820c3ff41519e26761e64f83e
    Former-commit-id: 333c9e13a9e03136c3f78bc7858f566cb9c8a6dc

--- data/himawari_to_modis.py
+++ data/himawari_to_modis.py
@@ -12,24 +12,28 @@ height = 2400
 
 hwidth = 12000
 hheight = 12000
-modis_list = glob.glob(os.path.join("modis", "*MOD09A1_*composite day*.nc"))
-modis_ndvi_list = glob.glob(os.path.join("modis", "*MOD09A1_*NDVI*.nc"))
-himawari_doy = []
-# turn himawari dates into day of year
-himawari_list = glob.glob(os.path.join("himawari", "*ndvi.tif"))
-for h in himawari_list:
-    dataset = gdal.Open(h, gdal.GA_ReadOnly)
-    a = dataset.GetRasterBand(1).ReadAsArray()
-    himawari_doy.append((datetime.strptime(h.split("\\")[-1].split(".")[0], "%Y%m%d%H%M").timetuple().tm_yday, a))
-base = himawari_doy[0][0]
-himawari_ndvi = np.zeros((hwidth, hheight, len(himawari_doy)))
-for t in himawari_doy:
-    himawari_ndvi[:, :, t[0] - base] = t[1]
-himawari_ndvi = xarray.DataArray(himawari_ndvi, dims=['x', 'y', 't'])
-for f in modis_list:
-    ds = xarray.open_dataset(f)
-    doy = int(f.split("_")[1][5:8])
-    ds -= doy  # normalize the day to 0
-    himawari_composite = himawari_ndvi.isel(x=np.arange(2400), y=np.arange(2400), t=ds.day_of_year.clip(min=0))
-    h_ds = xarray.Dataset(dict(ndvi=himawari_composite))
-    h_ds.to_netcdf("himawari_" + f)
+
+if __name__ == '__main__':
+    modis_list = glob.glob(os.path.join("modis", "*MOD09A1_*composite day*.nc"))
+    modis_ndvi_list = glob.glob(os.path.join("modis", "*MOD09A1_*NDVI*.nc"))
+    himawari_doy = []
+    # turn himawari dates into day of year
+    himawari_list = glob.glob(os.path.join("himawari", "*ndvi.tif"))
+    for h in himawari_list:
+        dataset = gdal.Open(h, gdal.GA_ReadOnly)
+        a = dataset.GetRasterBand(1).ReadAsArray()
+        himawari_doy.append((datetime.strptime(h.split("\\")[-1].split(".")[0], "%Y%m%d%H%M").timetuple().tm_yday, a))
+    base = himawari_doy[0][0]
+    himawari_ndvi = np.zeros((width, height, 16))
+    for t in himawari_doy:
+        if t[1] is None:
+            continue
+        himawari_ndvi[:, :, t[0] - base] = t[1][:width, :height]
+    himawari_ndvi = xarray.DataArray(himawari_ndvi, dims=['x', 'y', 't'])
+    for f in modis_list:
+        ds = xarray.open_dataset(f)
+        doy = int(f.split("_")[1][5:8])
+        ds -= doy  # normalize the day to 0
+        himawari_composite = himawari_ndvi.sel(x=np.arange(2400), y=np.arange(2400), t=ds.day_of_year.clip(min=0))
+        h_ds = xarray.Dataset(dict(ndvi=himawari_composite))
+        h_ds.to_netcdf("himawari_" + f)
