17a83b66f81d12c87bb61dbc51d2e934f9b5ef2d
Matthew <sciguymjm@gmail.com>
2017-07-24 13:29:16 -0700

    Initial processing script
    Former-commit-id: 51638297e6de207a6f9b594f8aa305d3e30f3a63 [formerly 95c9295ab1d00485d02dc9eb754703b0857de2d9]
    Former-commit-id: 22beb388b276d1ea0cc0c148ba3b2e77e01f07b1
    Former-commit-id: 5385fc34674366acf35c45b590d7eda32edce9aa

--- scripts/main.py
+++ scripts/main.py
@@ -7,18 +7,18 @@ BATCH_SIZE = 1
 MAX_STEPS = 30e3
 
 def train(args, global_step):
-    pass
+	pass
 
 def main(args):
-    global_step = tf.Variable(0, trainable=False)
-    train(args, global_step)
+	global_step = tf.Variable(0, trainable=False)
+	train(args, global_step)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="")
-    parser.add_argument("--train", action="store_true", help="Train a new model")
-    parser.add_argument("-lr", type=float, default=LEARNING_RATE, help="Learning rate")
-    parser.add_argument("-max", type=int, default=MAX_STEPS, help="Max number of steps to take")
-    parser.add_argument("-check", type=str, default=".", help="Checkpoint directory")
-    args = parser.parse_args()
-    main(args)
+	parser = argparse.ArgumentParser(description="")
+	parser.add_argument("--train", action="store_true", help="Train a new model")
+	parser.add_argument("-lr", type=float, default=LEARNING_RATE, help="Learning rate")
+	parser.add_argument("-max", type=int, default=MAX_STEPS, help="Max number of steps to take")
+	parser.add_argument("-check", type=str, default=".", help="Checkpoint directory")
+	args = parser.parse_args()
+	main(args)
