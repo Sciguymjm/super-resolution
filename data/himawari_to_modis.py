6197acb2ed813d9b7b0878ead50d13357a1ce51b
M <sciguymjm@gmail.com>
2017-07-26 16:30:50 -0700

    Does actual proper composite
    Former-commit-id: 591422d4788498de920adeb7c994cdd810c751cd [formerly 14b826c329b0b0a7666474707c6ac6bf9e0245a7]
    Former-commit-id: 167a937c6bd59a0a55ebe52630a56729e1a17976
    Former-commit-id: 99c4ac9fd3e5a88e320f47538a47d8bb19500882

--- data/himawari_to_modis.py
+++ data/himawari_to_modis.py
@@ -3,7 +3,6 @@ import glob
 import os
 from datetime import datetime
 
-import gdal
 import numpy as np
 import xarray
 
@@ -19,10 +18,12 @@ modis_list = glob.glob(os.path.join("modis", "*MOD09A1_*composite day*.nc"))
 modis_ndvi_list = glob.glob(os.path.join("modis", "*MOD09A1_*EVI*.nc"))
 himawari_doy = []
 # turn himawari dates into day of year
-himawari_list = glob.glob(os.path.join("himawari", "*ndvi.tif.npy"))
+himawari_list = glob.glob(os.path.join("himawari", "*evi.tif.npy"))
 for h in himawari_list:
-	a = np.load(h)
-	himawari_doy.append((datetime.strptime(h.split("\\")[-1].split("/")[-1].split(".")[0], "%Y%m%d%H%M").timetuple().tm_yday, a))
+    a = np.load(h)
+    himawari_doy.append(
+        (datetime.strptime(h.split("\\")[-1].split("/")[-1].split(".")[0], "%Y%m%d%H%M").timetuple().tm_yday, a))
+himawari_doy = sorted(himawari_doy, key=lambda x: x[0])
 base = himawari_doy[0][0]
 himawari_ndvi = np.zeros((12000, 12000, 16))
 i = 0
@@ -35,19 +36,18 @@ for t in himawari_doy:
         continue
     print t[1].shape
     if t[0] - base >= 16:
-        break # we dont want to go past the 16th day of this cycle
+        break  # we dont want to go past the 16th day of this cycle
     himawari_ndvi[:, :, t[0] - base] = t[1]
 size = 1112
 for f in modis_list:
     ds = xarray.open_dataset(f)
     doy = int(f.split("_")[1][5:8])
     top_left = (
-    int(float(f.split("_")[5])) / 2, int(float(f.split("_")[6].split(".")[0])) / 2)  # TODO: make sure rounding is correct?
+        int(float(f.split("_")[5])) / 2,
+        int(float(f.split("_")[6].split(".")[0])) / 2)  # TODO: make sure rounding is correct?
     ds -= doy  # normalize the day to 0
-    himawari_composite = himawari_ndvi[
-        range(top_left[0], top_left[0] + 4800), range(top_left[1], top_left[1] + 4800), ds.day_of_year.clip(
-            min=0).values]
-    print ds.day_of_year.clip(min=0).values
+    i, j = np.ogrid[top_left[0]:top_left[0] + 4800, top_left[1]: top_left[1] + 4800]
+    himawari_composite = himawari_ndvi.clip(min=0)[i, j, ds.day_of_year.clip(min=0).values]
     # himawari_composite[(himawari_composite < 0) | (himawari_composite == 9999)] = np.nan
     # himawari_composite = himawari_ndvi.isel_points(x=np.arange(2400), y=np.arange(2400), t=ds.day_of_year.clip(min=0))
     h_da = xarray.DataArray(himawari_composite, coords=[range(4800), range(4800)], dims=["x", "y"])
