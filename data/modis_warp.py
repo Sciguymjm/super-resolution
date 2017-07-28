d5018c2ae57c3b678e1d422e28b6215c84d33b07
M <sciguymjm@gmail.com>
2017-07-27 17:09:17 -0700

    Expanded modis script to extract all 4 bands
    Former-commit-id: 6803e7683240b173cd0d42b061bc32131c231aa6 [formerly 69c32bc182e6c18bff6cce5efb66cdf095a3f7a1]
    Former-commit-id: abf2afa69b138398284cc9256eceb9a99cfafc64
    Former-commit-id: 87e006f43a883b34391e8a0d8a248570a9cd1413

--- data/modis_warp.py
+++ data/modis_warp.py
@@ -5,4 +5,7 @@ import glob
 flist = glob.glob("modis/*.hdf")
 
 for f in flist:
-    call("gdalwarp -overwrite -t_srs EPSG:3857 -r near -multi -dstnodata -9999 -of GTiff \"HDF4_EOS:EOS_GRID:\\\"" + f + "\\\":MODIS_Grid_16DAY_250m_500m_VI:250m 16 days NDVI\" " + f.split("/")[0] + "/warped-" + f.split("/")[1].replace("hdf", "tif"), shell=True)
+    call("gdalwarp -overwrite -t_srs EPSG:3857 -r near -multi -dstnodata -9999 -of GTiff \"HDF4_EOS:EOS_GRID:\\\"" + f + "\\\":MODIS_Grid_16DAY_250m_500m_VI:250m 16 days NDVI\" " + f.split("/")[0] + "/warped-ndvi-" + f.split("/")[1].replace("hdf", "tif"), shell=True)
+    call("gdalwarp -overwrite -t_srs EPSG:3857 -r near -multi -dstnodata -9999 -of GTiff \"HDF4_EOS:EOS_GRID:\\\"" + f + "\\\":MODIS_Grid_16DAY_250m_500m_VI:250m 16 days EVI\" " + f.split("/")[0] + "/warped-evi-" + f.split("/")[1].replace("hdf", "tif"), shell=True)
+    call("gdalwarp -overwrite -t_srs EPSG:3857 -r near -multi -dstnodata -9999 -of GTiff \"HDF4_EOS:EOS_GRID:\\\"" + f + "\\\":MODIS_Grid_16DAY_250m_500m_VI:250m 16 days NIR reflectance\" " + f.split("/")[0] + "/warped-nir-" + f.split("/")[1].replace("hdf", "tif"), shell=True)
+    call("gdalwarp -overwrite -t_srs EPSG:3857 -r near -multi -dstnodata -9999 -of GTiff \"HDF4_EOS:EOS_GRID:\\\"" + f + "\\\":MODIS_Grid_16DAY_250m_500m_VI:250m 16 days composite day of the year\" " + f.split("/")[0] + "/warped-cdoy-" + f.split("/")[1].replace("hdf", "tif"), shell=True)
