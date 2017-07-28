9b4c4fca2ab9e382357c10d0b00fdab6cbb28191
M <sciguymjm@gmail.com>
2017-07-28 14:57:00 -0700

    Big overhaul of h2m script. Right now does not work due to discrepancies between resolution / dimensions of modis + himawari
    Former-commit-id: eeafc51043a2ba8b151d843b625c206e5e29734c

--- data/himawari_to_modis.py
+++ data/himawari_to_modis.py
@@ -3,8 +3,10 @@ import glob
 import os
 from datetime import datetime
 
+import gdal
 import numpy as np
-import xarray
+from osgeo import gdal as osgd
+from osgeo import osr
 
 testing = False
 
@@ -14,44 +16,40 @@ height = 4800 / 2
 hwidth = 12000
 hheight = 12000
 
-modis_list = glob.glob(os.path.join("modis", "*MOD09A1_*composite day*.nc"))
-modis_ndvi_list = glob.glob(os.path.join("modis", "*MOD09A1_*NDVI*.nc"))
+modis_list = glob.glob(os.path.join("modis", "*cdoy*MOD13Q1*.tif"))
+modis_ndvi_list = glob.glob(os.path.join("modis", "*ndvi*MOD13Q1*.tif"))
 himawari_doy = []
 # turn himawari dates into day of year
-himawari_list = glob.glob(os.path.join("himawari", "*ndvi.tif.npy"))
+himawari_list = glob.glob(os.path.join("himawari", "warped-*ndvi.tif"))
 for h in himawari_list:
-    a = np.load(h)
+    a = gdal.Open(h)
     himawari_doy.append(
-        (datetime.strptime(h.split("\\")[-1].split("/")[-1].split(".")[0], "%Y%m%d%H%M").timetuple().tm_yday, a))
+        (datetime.strptime(h.split("\\")[-1].split("/")[-1].split(".")[0].split("-")[1],
+                           "%Y%m%d%H%M").timetuple().tm_yday, a))
 himawari_doy = sorted(himawari_doy, key=lambda x: x[0])
 base = himawari_doy[0][0]
-himawari_ndvi = np.zeros((12000, 12000, 16))
-i = 0
-for t in himawari_doy:
-    if testing:
-        himawari_ndvi[:, :, i] = i
-        i += 1
-        continue
-    if t[1] is None:
-        continue
-    print t[1].shape
-    if t[0] - base >= 16:
-        break  # we dont want to go past the 16th day of this cycle
-    himawari_ndvi[:, :, t[0] - base] = t[1]
-size = 1112
+# himawari_ndvi = np.ndarray(shape=himawari_doy[0][1].ReadAsArray().shape + (16,))
 for f in modis_list:
-    ds = xarray.open_dataset(f)
-    doy = int(f.split("_")[1][5:8])
-    top_left = (
-        int(float(f.split("_")[5])) / 2,
-        int(float(f.split("_")[6].split(".")[0])) / 2)  # TODO: make sure rounding is correct?
-    ds -= doy  # normalize the day to 0
-    i, j = np.ogrid[top_left[0] + 3000:top_left[0] + 3000 + 2400, top_left[1]: top_left[1] + 2400]
-    himawari_composite = himawari_ndvi.clip(min=0)[i, j, ds.day_of_year.clip(min=0).values[::2, ::2]]
-    # himawari_composite[(himawari_composite < 0) | (himawari_composite == 9999)] = np.nan
-    # himawari_composite = himawari_ndvi.isel_points(x=np.arange(2400), y=np.arange(2400), t=ds.day_of_year.clip(min=0))
-    h_da = xarray.DataArray(himawari_composite, coords=[range(2400), range(2400)], dims=["x", "y"])
-    h_ds = xarray.Dataset(dict(ndvi=h_da))
-    h_ds.to_netcdf(
-        f.split("/")[0] + "/himawari_" + "_".join(f.split("/")[1].split("_")[3:]) + f.split("/")[1].split("_")[
-            1] + ".nc")
+    m_ds = gdal.Open(f)  # open warped modis array
+    shape = m_ds.ReadAsArray().shape  # read shape to form ogrid
+    modis_data = m_ds.ReadAsArray().clip(min=0)
+    ulx, xres, xskew, uly, yskew, yres = m_ds.GetGeoTransform()  # calculate top left and bottom right
+    lrx = ulx + (m_ds.RasterXSize * xres)
+    lry = uly + (m_ds.RasterYSize * yres)
+    h_cutout = np.zeros((shape[0], shape[1], 16))  # this is for all of the himawari cutouts
+    for i, h in enumerate(himawari_doy):
+        gdal.Warp("temp.tif", h[1], options=osgd.WarpOptions(outputBounds=(ulx, lry, lrx, uly)))  # warp to file
+        # read file to array, since im not sure how the 'destarray' function works in Warp
+        h_cutout[:, :, i] = gdal.Open("temp.tif").ReadAsArray().astype(dtype="int")
+    i, j = np.ogrid[:shape[1], :shape[0]]
+    himawari_composite = h_cutout[i, j, modis_data]
+    driver = gdal.GetDriverByName('GTiff')
+    outRaster = driver.Create("composite-" + f, shape[0], shape[1], 1, gdal.GDT_Int16)
+    outRaster.SetGeoTransform(m_ds.GetGeoTransform())
+    outband = outRaster.GetRasterBand(1)
+    outband.SetNoDataValue(-9999)
+    outband.WriteArray(himawari_composite)
+    outRasterSRS = osr.SpatialReference()
+    outRasterSRS.ImportFromEPSG(3857)
+    outRaster.SetProjection(outRasterSRS.ExportToWkt())
+    outband.FlushCache()
