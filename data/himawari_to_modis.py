5c278b4dc0fc2f16c5a365d53240702f9bd3c415
M <sciguymjm@gmail.com>
2017-07-31 17:55:28 -0700

    added argparse and some more overhaul
    Former-commit-id: aa1ad269e12a9c98b38824f1d482719321fe2eeb [formerly 6069f4708883825555c7798fd4cda0203825339b]
    Former-commit-id: 995556dc8ae4839045d81437439977640e7410a9
    Former-commit-id: 07cc03c9907382a70f2c3705aaf76f8b8cc5a9c4

--- data/himawari_to_modis.py
+++ data/himawari_to_modis.py
@@ -4,38 +4,41 @@ import os
 from datetime import datetime
 
 import gdal
-from subprocess import Popen
 import numpy as np
 from osgeo import osr
 
-testing = False
+import argparse
 
-width = 4800 / 2
-height = 4800 / 2
+parser = argparse.ArgumentParser()
+parser.add_argument("-sl", help="Stack layers", action="store_true")
+parser.add_argument("-t", help="Test - do not load data, only use placeholders", action="store_true")
+args = parser.parse_args()
+stack_layers = args.sl
+testing = args.t
 
-hwidth = 12000
-hheight = 12000
 modis_ndvi_file = glob.glob(os.path.join("modis", "ndvi*.tif"))[0]  # merged file
 modis_cdoy_file = glob.glob(os.path.join("modis", "cdoy*.tif"))[0]  # merged file
-# modis_list = glob.glob(os.path.join("modis", "*cdoy*MOD13Q1*.tif"))
-# modis_ndvi_list = glob.glob(os.path.join("modis", "*ndvi*MOD13Q1*.tif"))
 himawari_doy = []
+if stack_layers:
+    himawari_list = glob.glob(os.path.join("himawari", "warped-*ndvi.tif"))
+    for h in himawari_list:
+        options = gdal.BuildVRTOptions(separate=True)
+        out = gdal.BuildVRT("out.vrt", [h, modis_ndvi_file, modis_cdoy_file], options=options)
+        gdal.Translate("himawari/output-" + h.split("/")[1], out)
 # turn himawari dates into day of year
-himawari_list = glob.glob(os.path.join("himawari", "warped-*ndvi.tif"))
-for h in himawari_list:
-    out = gdal.BuildVRT("out.vrt", [h, modis_ndvi_file, modis_cdoy_file])
-    gdal.Translate( "himawari/output-" + h.split("/")[1], out)
-    #Popen("gdal_merge.py -init \"-9999\" -separate -of GTiff -o himawari/output-" + h.split("/")[1] + " " + modis_ndvi_file + " " + modis_cdoy_file, shell=True).wait()
-for h in himawari_list:
+merged_list = glob.glob(os.path.join("himawari", "output-*.tif"))
+for h in merged_list:
     a = gdal.Open(h)
     himawari_doy.append(
-        (datetime.strptime(h.split("\\")[-1].split("/")[-1].split(".")[0].split("-")[1],
+        (datetime.strptime(h.split("\\")[-1].split("/")[-1].split(".")[0].split("-")[2],
                            "%Y%m%d%H%M").timetuple().tm_yday, a))
+modis_ndvi = himawari_doy[0][1].GetRasterBand(2).ReadAsArray()  # bands: himawari, modis, modis_doy
+modis_doy = himawari_doy[0][1].GetRasterBand(3).ReadAsArray()
 himawari_doy = sorted(himawari_doy, key=lambda x: x[0])
 base = himawari_doy[0][0]
-h_shape = himawari_doy[0][1].ReadAsArray().shape
+h_shape = himawari_doy[0][1].GetRasterBand(1).ReadAsArray().shape
 layers = 16
-himawari_ndvi = np.zeros((h_shape[0], h_shape[1], 4))
+himawari_ndvi = np.zeros((h_shape[0], h_shape[1], layers))
 i = 0
 for t in himawari_doy:
     if testing:
@@ -46,16 +49,15 @@ for t in himawari_doy:
         continue
     if t[0] - base >= layers:
         break  # we dont want to go past the 16th day of this cycle
-    himawari_ndvi[:, :, t[0] - base] = t[1].ReadAsArray()
+    himawari_ndvi[:, :, t[0] - base] = t[1].GetRasterBand(1).ReadAsArray()
 # open warped modis array
-m_ds = gdal.Open(modis_cdoy_file)
-shape = m_ds.ReadAsArray().shape  # read shape to form ogrid
+shape = modis_doy.shape  # read shape to form ogrid
 i, j = np.ogrid[:shape[0], :shape[1]]
-modis_data = m_ds.ReadAsArray().clip(min=0)
-himawari_composite = himawari_ndvi[i, j, modis_data]
+modis_doy = modis_doy.clip(min=0) # TODO: mask himawari properly
+himawari_composite = himawari_ndvi[i, j, modis_doy]
 driver = gdal.GetDriverByName('GTiff')
 outRaster = driver.Create("composite-" + modis_ndvi_file, shape[0], shape[1], 1, gdal.GDT_Int16)
-outRaster.SetGeoTransform(m_ds.GetGeoTransform())
+outRaster.SetGeoTransform(himawari_doy[0][1].GetRasterBand(2).GetGeoTransform())
 outband = outRaster.GetRasterBand(1)
 outband.SetNoDataValue(-9999)
 outband.WriteArray(himawari_composite)
