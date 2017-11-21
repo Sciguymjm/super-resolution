56300321136bf6f431148a92da3e8f4eec2db950
M <sciguymjm@gmail.com>
2017-11-21 10:30:37 -0800

    some multiprocessing

--- data/download_modis.py
+++ data/download_modis.py
@@ -12,13 +12,13 @@ args = parser.parse_args()
 dry_run = args.dr
 username = "mmage"
 password = "ue3AaxCIb23I"
-hMin = 7
+hMin = 6
 hMax = 15
-vMin = 3
+vMin = 1
 vMax = 13
 for h in range(hMin, hMax + 1):
     for v in range(vMin, vMax + 1):
-        s = "wget -P modis-daily/ -r -np -nH -R index.html https://e4ftl01.cr.usgs.gov/MOLT/MOD09GQ.006/2017.11.14/ --user mmage --password ue3AaxCIb23I -nd -A \"*h" + str(
+        s = "wget -P modis/ -r -np -nH -R index.html https://e4ftl01.cr.usgs.gov/MOLT/MOD13Q1.006/2017.09.30/ --user mmage --password ue3AaxCIb23I -nd -A \"*h" + str(
             h).zfill(2) + "v" + str(v).zfill(2) + "*.hdf\""
         if dry_run:
             print s
--- data/goes-composite.py
+++ data/goes-composite.py
@@ -1,4 +1,3 @@
-
 # coding: utf-8
 
 # # Estimation of MODIS-like Surface-Spectral Reflectance from Geostationary Satellites using Deep Neural Networks 
@@ -7,221 +6,259 @@
 
 # In[1]:
 
-import argparse
 import datetime
 import glob
 import os
 
 import gdal
-import numpy as np
-from IPython.display import Image
-import matplotlib.pyplot as plt
 import matplotlib
+import numpy as np
+
 matplotlib.rcParams['figure.figsize'] = "8,8"
 import scipy.ndimage
-gdal.VersionInfo()
+from multiprocessing import Pool
 
 
-# In[2]:
+def proc(day):
+    goes_selected = sorted(glob.glob('goes/{}/{}/O*C03*.nc'.format(day, timendate.hour)))
+    if len(goes_selected) < 1:
+        os.system(
+            'aws s3 sync s3://noaa-goes16/ABI-L1b-RadF/2017/{}/ goes/{}/ --exclude "*" --include "*M3C03_*2017{}{}*"'.format(
+                day, day, day,
+                timendate.strftime('%H%M')))
+    goes_selected = sorted(glob.glob('goes/{}/{}/O*C03*.nc'.format(day, timendate.hour)))
+    temp = []
+    for i, fi in enumerate(goes_selected):
+        f = os.path.basename(fi)
+        gtime = f[34:38]
+        ftime = datetime.datetime.strptime(gtime, '%H%M')
+        if ftime.hour == timendate.hour and ftime.minute == timendate.minute:
+            temp.append(fi)
+    return day, temp
 
 
-raw_dir = "modis"
-layers = ["250m 16 days composite day of the year", # doy 1st for compositing reasons
-          "250m 16 days NDVI", 
-          "250m 16 days NIR reflectance"] 
-raw_files = glob.glob(raw_dir + '/*.hdf')
+def warp_goes(r):
+    i, day = r
+    if len(goes_dict[day]) > 0:
+        f = goes_dict[day][0]  # veggie NIR
+        output = os.path.dirname(f) + '/' + os.path.basename(f).split('.')[0] + '.tif'
+
+        if not os.path.isfile(output):
+            t = gdal.Warp(output, 'NETCDF:' + f + ':Rad', dstSRS='EPSG:3857')
+            print output
 
 
-# In[3]:
+if __name__ == '__main__':
 
+    raw_dir = "modis"
+    layers = ["250m 16 days composite day of the year",  # doy 1st for compositing reasons
+              "250m 16 days NDVI",
+              "250m 16 days NIR reflectance"]
+    raw_files = glob.glob(raw_dir + '/*.hdf')
 
-
-fs = [f for f in raw_files if 'A2017257' in f] # set day here
-day_of_year = int(fs[0].split('.')[4][4:7])
-
-
-# In[4]:
-for filepath in fs:
-
-    ds = gdal.Open(filepath, gdal.GA_ReadOnly)
-    datasets = ds.GetSubDatasets()
-    datasets[:2]
-
-
-    # ## Day of Year
-
-    # In[5]:
-
-
-    doy_layer = ""
-    for dataset in datasets:
-        if layers[0] in dataset[0]:
-            doy_layer = dataset[0]
-    file_type = doy_layer.split(' ')[3].lower()
-    doy_layer, file_type
-
-
-    # In[6]:
-
-
-    doy_modis = gdal.Open(doy_layer)
+    # In[3]:
 
 
 
-    # In[7]:
+    fs = [f for f in raw_files if 'A2017273' in f]  # set day here
+    day_of_year = int(fs[0].split('.')[4][4:7])
+
+    # In[4]:
+    for filepath in fs:
+
+        ds = gdal.Open(filepath, gdal.GA_ReadOnly)
+        datasets = ds.GetSubDatasets()
+        datasets[:2]
+
+        # ## Day of Year
+
+        # In[5]:
 
 
-    output = filepath.replace('.hdf', '.' + file_type + ".tif")
-    gdal.Warp(output, doy_modis, dstSRS='EPSG:3857')
-    data = gdal.Open(output)
-    doy_arr = data.ReadAsArray()
+        doy_layer = ""
+        for dataset in datasets:
+            if layers[0] in dataset[0]:
+                doy_layer = dataset[0]
+        file_type = doy_layer.split(' ')[3].lower()
+        doy_layer, file_type
+
+        # In[6]:
 
 
+        doy_modis = gdal.Open(doy_layer)
 
-    # ## Near Infrared
-
-    # In[8]:
-
-
-    nir_layer = ""
-    for dataset in datasets:
-        if layers[2] in dataset[0]:
-            nir_layer = dataset[0]
-
-    file_type = nir_layer.split(' ')[3].lower()
-    print nir_layer, file_type
+        # In[7]:
 
 
-    # In[9]:
+        output = filepath.replace('.hdf', '.' + file_type + ".tif")
+        gdal.Warp(output, doy_modis, dstSRS='EPSG:3857', xRes=1000, yRes=1000, resampleAlg='near')
+        data = gdal.Open(output)
+        doy_arr = data.ReadAsArray()
+
+        # ## Near Infrared
+
+        # In[8]:
 
 
-    nir_modis = gdal.Open(nir_layer)
-    nir_modis.ReadAsArray()
+        nir_layer = ""
+        for dataset in datasets:
+            if layers[2] in dataset[0]:
+                nir_layer = dataset[0]
+
+        file_type = nir_layer.split(' ')[3].lower()
+        print nir_layer, file_type
+
+        # In[9]:
+
+        nir_modis = gdal.Open(nir_layer)
+        nir_modis.ReadAsArray()
+
+        # In[10]:
 
 
-    # In[10]:
+        output = filepath.replace('.hdf', '.' + file_type + ".tif")
+        gdal.Warp(output, nir_modis, dstSRS='EPSG:3857', xRes=1000, yRes=1000, resampleAlg='near')
+        data = gdal.Open(output)
+        nir_arr = data.ReadAsArray()
+
+        # In[11]:
 
 
-    output = filepath.replace('.hdf', '.' + file_type + ".tif")
-    gdal.Warp(output, nir_modis, dstSRS='EPSG:3857')
-    data = gdal.Open(output)
-    nir_arr = data.ReadAsArray()
+        ulx, xres, xskew, uly, yskew, yres = data.GetGeoTransform()
+        lrx = ulx + (data.RasterXSize * xres)
+        lry = uly + (data.RasterYSize * yres)
+        srs = data.GetProjection()
 
-    # In[11]:
+        # In[12]:
+        # t = gdal.Warp('test5.tif', 'NETCDF:goes/257/18/OR_ABI-L1b-RadF-M3C02_G16_s20172571800379_e20172571811146_c20172571811182.nc:Rad', dstSRS='EPSG:3857')
+        cut = gdal.Warp('cutout.tif', gdal.Open('test5.tif'), outputBoundsSRS=srs, outputBounds=[ulx, lry, lrx, uly])
+        culx, cxres, cxskew, culy, cyskew, cyres = cut.GetGeoTransform()
+        # ## GOES-16 NIR
 
-
-    ulx, xres, xskew, uly, yskew, yres = data.GetGeoTransform()
-    lrx = ulx + (data.RasterXSize * xres)
-    lry = uly + (data.RasterYSize * yres)
-    srs = data.GetProjection()
-
-
-    # In[12]:
-    # t = gdal.Warp('test5.tif', 'NETCDF:goes/257/18/OR_ABI-L1b-RadF-M3C02_G16_s20172571800379_e20172571811146_c20172571811182.nc:Rad', dstSRS='EPSG:3857')
-    cut = gdal.Warp('cutout.tif', gdal.Open('test5.tif'), outputBoundsSRS=srs, outputBounds=[ulx, lry, lrx, uly])
-    culx, cxres, cxskew, culy, cyskew, cyres = cut.GetGeoTransform()
-    # ## GOES-16 NIR
-
-    # In[13]:
+        # In[13]:
 
 
-    doy = int(filepath.split('.')[1][5:])
-    year = int(filepath.split('.')[4][0:4])
-    date = datetime.datetime(year, 1, 1) + datetime.timedelta(doy - 1)
+        doy = int(filepath.split('.')[1][5:])
+        year = int(filepath.split('.')[4][0:4])
+        date = datetime.datetime(year, 1, 1) + datetime.timedelta(doy - 1)
 
-    # Find closest goes-16 times
-    modis_time = filepath.split('.')[4][7:]
-    _time = datetime.datetime.strptime(modis_time, "%H%M%S")
-    if _time.minute % 15 > 7:
-        _time += datetime.timedelta(minutes=15 - (_time.minute % 15))
-    else:
-        _time += datetime.timedelta(minutes=-_time.minute % 15)
-    timendate = datetime.datetime.combine(date.date(), _time.time())
-    timendate.strftime('%Y-%m-%d %H:%M')
+        offsets = {
+            '00': 240,
+            '01': 120,
+            '02': 80,
+            '03': 60,
+            '04': 51,
+            '05': 45,
+            '06': 42,
+            '07': 40,
+            '08': 40,
+            '09': 40,
+            '10': 40,
+            '11': 42,
+            '12': 45,
+            '13': 51,
+            '14': 60,
+            '15': 80,
+            '16': 120,
+            '17': 240
+        }
 
-    goes_dict = {}
-    for day in range(timendate.timetuple().tm_yday, timendate.timetuple().tm_yday + 16):
-        goes_selected = sorted(glob.glob('goes/{}/{}/O*C03*.nc'.format(day, timendate.hour)))
-        if len(goes_selected) < 1:
-            os.system(
-                'aws s3 sync s3://noaa-goes16/ABI-L1b-RadF/2017/{}/ goes/{}/ --exclude "*" --include "*M3C03_*2017{}{}*"'.format(
-                    day, day, day,
-                    timendate.strftime('%H%M')))
-        goes_selected = sorted(glob.glob('goes/{}/{}/O*C03*.nc'.format(day, timendate.hour)))
-        temp = []
-        for i, fi in enumerate(goes_selected):
-            f = os.path.basename(fi)
-            gtime = f[34:38]
-            ftime = datetime.datetime.strptime(gtime, '%H%M')
-            if ftime.hour == timendate.hour and ftime.minute == timendate.minute:
-                temp.append(fi)
-        goes_dict[day] = temp
-    print goes_dict, timendate
+        # Find closest goes-16 times
+        modis_time = filepath.split('.')[4][7:]
+        # _time = datetime.datetime.strptime(modis_time, "%H%M%S") + datetime.timedelta(hours=5)
+        _time = datetime.time(14, 15, 0)  # UTC
+        ft = filepath.split('.')[2]
+        h = ft[1:3]
+        v = ft[4:6]
+        # _time += datetime.timedelta(minutes=offsets[v] * int(h))
+        #
+        # if _time.minute % 15 > 7:
+        #     _time += datetime.timedelta(minutes=15 - (_time.minute % 15))
+        # else:
+        #     _time += datetime.timedelta(minutes=-_time.minute % 15)
+        timendate = datetime.datetime.combine(date.date(), _time)
+        timendate.strftime('%Y-%m-%d %H:%M')
 
+        p = Pool(4)
+        result = p.map(proc, list(range(timendate.timetuple().tm_yday, timendate.timetuple().tm_yday + 16)))
+        print result
+        goes_dict = {x: y for x, y in result}
+        print goes_dict, timendate
 
-    # resize using nn
-    resized_nir = scipy.ndimage.zoom(data.ReadAsArray(), data.GetGeoTransform()[1] / cxres, order=0)
-    shape = resized_nir.shape
-    matrix = np.zeros((shape[0], shape[1], 16))
+        test_run = False
+        p = Pool(4)
+        p.map(warp_goes, list(enumerate(goes_dict)))
 
-    test_run = False
-    for i, day in enumerate(goes_dict):
-        if len(goes_dict[day]) > 0:
-            f = goes_dict[day][0]  # veggie NIR
-            output = os.path.dirname(f) + '/' + os.path.basename(f).split('.')[0] + '.tif'
+        ratio = data.GetGeoTransform()[1] / cxres
 
-            if not os.path.isfile(output):
-                t = gdal.Warp(output, 'NETCDF:' + f + ':Rad', dstSRS='EPSG:3857')
-                print output
-            else:
+        # resize using nn
+        resized_nir = scipy.ndimage.zoom(data.ReadAsArray(), ratio, order=0)
+        shape = resized_nir.shape
+        matrix = np.zeros((shape[0], shape[1], 16))
+
+        for r in enumerate(goes_dict):
+            i, day = r
+            if len(goes_dict[day]) > 0:
+                f = goes_dict[day][0]  # veggie NIR
+                output = os.path.dirname(f) + '/' + os.path.basename(f).split('.')[0] + '.tif'
                 t = gdal.Open(output)
-            cut = gdal.Warp('cutout.tif', t, outputBoundsSRS=srs, outputBounds=[ulx, lry, lrx, uly])
-            culx, cxres, cxskew, culy, cyskew, cyres = cut.GetGeoTransform()
-            arr = cut.ReadAsArray()
-            assert arr.shape == shape, "{} is not equal to {}".format(arr.shape, shape)
-            if test_run:
-                for x in range(16):
-                    matrix[:, :, x] = arr
-                break
-            else:
-                matrix[:, :, i] = arr
-    for layer in range(16):
-        matrix[:, :, layer][resized_nir == -1000] = -1000
+                if not os.path.isfile(output):
+                    t = gdal.Warp(output, 'NETCDF:' + f + ':Rad', dstSRS='EPSG:3857')
+                    print output
+                else:
+                    t = gdal.Open(output)
+                cut = gdal.Warp('cutout.tif', t, outputBoundsSRS=srs, outputBounds=[ulx, lry, lrx, uly])
+                culx, cxres, cxskew, culy, cyskew, cyres = cut.GetGeoTransform()
+                arr = cut.ReadAsArray()
+                print "shape", arr.shape, nir_arr.shape
+                assert arr.shape == shape, "{} is not equal to {}".format(arr.shape, shape)
+                if test_run:
+                    for x in range(16):
+                        matrix[:, :, x] = arr
+                    continue
+                else:
+                    matrix[:, :, i] = arr
+        for layer in range(16):
+            matrix[:, :, layer][resized_nir == -1000] = -1000
 
-    # Resize doy array
-    # resize using nn
-    resized_doy = scipy.ndimage.zoom(doy_arr, data.GetGeoTransform()[1] / cxres, order=0)
-    print 'Ratio: ', xres / cxres
+        # Resize doy array
+        # resize using nn
+        resized_doy = scipy.ndimage.zoom(doy_arr, ratio, order=0)
+        print 'Ratio: ', ratio
 
-    i, j = np.ogrid[:shape[0], :shape[1]]
-    resized_doy -= doy
-    resized_doy = resized_doy.clip(min=0)
-    composite = matrix[i, j, resized_doy]
+        i, j = np.ogrid[:shape[0], :shape[1]]
+        resized_doy -= doy
+        resized_doy = resized_doy.clip(min=0)
+        composite = matrix[i, j, resized_doy]
 
 
-    def array_to_raster_w_source(dst_filename, array, source):
-        """Array > Raster
-        Save a raster from a C order array using another dataset as source.
+        def array_to_raster_w_source(dst_filename, array, source):
+            """Array > Raster
+            Save a raster from a C order array using another dataset as source.
 
-        :param array: ndarray
-        """
-        driver = gdal.GetDriverByName('GTiff')
+            :param array: ndarray
+            """
+            driver = gdal.GetDriverByName('GTiff')
 
-        dataset = driver.Create(
-            dst_filename,
-            array.shape[1],
-            array.shape[0],
-            1,
-            gdal.GDT_Float32)
+            dataset = driver.Create(
+                dst_filename,
+                array.shape[1],
+                array.shape[0],
+                1,
+                gdal.GDT_Float32)
 
-        dataset.SetGeoTransform(source.GetGeoTransform())
-        dataset.GetRasterBand(1).WriteArray(array)
-        dataset.GetRasterBand(1).SetNoDataValue(-1000)  ##if you want these values transparent
-        dataset.SetGeoTransform(source.GetGeoTransform())
-        dataset.SetProjection(source.GetProjection())
-        dataset.FlushCache()  # Write to disk.
+            dataset.SetGeoTransform(source.GetGeoTransform())
+            dataset.GetRasterBand(1).WriteArray(array)
+            dataset.GetRasterBand(1).SetNoDataValue(-1000)  ##if you want these values transparent
+            dataset.SetGeoTransform(source.GetGeoTransform())
+            dataset.SetProjection(source.GetProjection())
+            dataset.FlushCache()  # Write to disk.
 
-    fn = 'composite/{}/{}.tif'.format(doy, filepath.split('.')[2])
-    if not os.path.exists(os.path.dirname(fn)):
-        os.makedirs(os.path.dirname(fn))
-    array_to_raster_w_source(fn, composite, cut)  # use the cutout for proper resolution
+
+        fn = 'composite/{}/{}.tif'.format(doy, filepath.split('.')[2])
+        if not os.path.exists(os.path.dirname(fn)):
+            os.makedirs(os.path.dirname(fn))
+        array_to_raster_w_source(fn, composite, cut)  # use the cutout for proper resolution
+    os.system(
+        r'gdal_merge.py -n -1000 -a_nodata -1000 -of GTiff -o composite\{}\composite-{}.tif composite\{}\h*.tif'.format(
+            doy, doy, doy))
